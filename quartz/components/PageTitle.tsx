41e8862959b82285f6b2ed68485def3704a21152
mohnad banat <75941340+mohnad-0b@users.noreply.github.com>
2025-01-23 12:02:55 +0300

    Update PageTitle.tsx

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -5,12 +5,21 @@ import { i18n } from "../i18n"
 
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
-  const baseDir = pathToRoot(fileData.slug!)
+  const baseDir = fileData?.slug ? pathToRoot(fileData.slug) : "#"
+
   return (
-    <img src="https://yt3.googleusercontent.com/eS16DR9GFPaYPWV4bDleNEw1YxSw-uttCpsk-NJ5dlLd6gCj4t7lcYGF92MYNzFVvoJU5weWUg=s160-c-k-c0x00ffffff-no-rj" height="150" class="profile" width="150">
-    <h2 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>{title}</a>
-    </h2>
+    <div>
+      <img
+        src="https://yt3.googleusercontent.com/eS16DR9GFPaYPWV4bDleNEw1YxSw-uttCpsk-NJ5dlLd6gCj4t7lcYGF92MYNzFVvoJU5weWUg=s160-c-k-c0x00ffffff-no-rj"
+        height="150"
+        className="profile"
+        width="150"
+        alt="Profile"
+      />
+      <h2 className={classNames(displayClass, "page-title")}>
+        <a href={baseDir}>{title}</a>
+      </h2>
+    </div>
   )
 }
 
@@ -18,6 +27,11 @@ PageTitle.css = `
 .page-title {
   font-size: 1.75rem;
   margin: 0;
+}
+.profile {
+  border-radius: 50%;
+  display: block;
+  margin: 1rem auto;
 }
 `
 
