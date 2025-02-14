8a9bbd1e6dbc826e22763770ed3a098b3c5b629c
mohnad-0b <mohnadbanat00@gamil.com>
2025-02-14 22:14:37 +0300

    fix og:image

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "mohnad-0b.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: true,
