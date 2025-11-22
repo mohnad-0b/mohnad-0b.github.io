48858ccbc1e02d3b00fc51de6e138d1ea03e2544
Mohannad Banat <75941340+mohnad-0b@users.noreply.github.com>
2025-11-22 16:53:38 +0300

    Enhance 404 page with custom styling and image
    Signed-off-by: Mohannad Banat <75941340+mohnad-0b@users.noreply.github.com>

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -8,8 +8,12 @@ const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
 
   return (
     <article class="popover-hint">
-      <h1>404</h1>
-      <p>{i18n(cfg.locale).pages.error.notFound}</p>
+      <h1 style="font-size: 4rem; margin-bottom: 0.5rem; text-align: center;">4 Oh! 4</h1>
+      <p style="font-size: 1.2rem; margin-bottom: 1rem; font-weight: 700;">
+        {i18n(cfg.locale).pages.error.notFound}
+      </p>
+      <img src="https://i.pinimg.com/736x/e6/17/78/e617787f593ac0e7ca11f553dd95d9b1.jpg" alt="404 Error" style="max-width: 100%; height: auto;" />
+      <br />
       <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
     </article>
   )
